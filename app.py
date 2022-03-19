a41edfc1d138c38eef69e38bc5124dda8121a184
Daniel Lertsirikarn <mightlert@hotmail.com>
2022-03-19 11:57:12 -0400

    updates

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask,render_template,request,send_file
 from TTS.bin.mightTTS import mainsynthesize
-myapp = Flask(__name__)
+app = Flask(__name__)
 
-@myapp.route("/")
-def myapp():
+@app.route("/")
+def index():
     
     return render_template('inputform.html')
 
 
 
  
-@myapp.route('/outputform/', methods = ['GET','POST'])
+@app.route('/outputform', methods = ['POST'])
 def outputform():
-   if request.method == 'GET':
+    if request.method == 'GET':
         return f"ERROR called GET instead of POST"
     if request.method == 'POST':
         input_form_data = request.form
@@ -26,4 +26,6 @@ def outputform():
         outputmeesage = "Finished synthesizing: " + inputtext
 
         return outputmeesage
- 
+
+if __name__ == '__main__':
+	app.run()
