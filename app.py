594da67a09cfa0e9a7eb42a754befce517130733
Daniel Lertsirikarn <mightlert@hotmail.com>
2022-03-19 09:21:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from TTS.bin.mightTTS import mainsynthesize
 myapp = Flask(__name__)
 
 @myapp.route("/")
-def hello():
+def myapp():
     
     return render_template('inputform.html')
 
 
 
  
-@myapp.route('/outputform/', methods = ['POST', 'GET'])
+@myapp.route('/outputform/', methods = ['POST'])
 def outputform():
  #   if request.method == 'GET':
  #       return f"The URL /data is accessed directly. Try going to 'homepage' to submit form"
-    if request.method == 'POST' or request.method == 'GET':
+    if request.method == 'POST':
         input_form_data = request.form
         inputtext=request.form['inputtext']
         mainsynthesize(inputtext)
